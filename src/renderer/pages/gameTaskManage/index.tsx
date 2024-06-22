637ae51f565c2bdfc735de0a2fa0447e473fd5ad
unknown <962965494@qq.com>
2024-06-22 12:29:19 +0800

    黄金罗盘

--- src/renderer/pages/gameTaskManage/index.tsx
+++ src/renderer/pages/gameTaskManage/index.tsx
@@ -114,6 +114,7 @@ export default function GameTaskManage() {
               <Select.Option value="风月谷">风月谷</Select.Option>
               <Select.Option value="北海沙滩">北海沙滩</Select.Option>
               <Select.Option value="揽仙镇外">揽仙镇外</Select.Option>
+              <Select.Option value="官道南">官道南</Select.Option>
             </Select>
           </Modal>
         </Space>
